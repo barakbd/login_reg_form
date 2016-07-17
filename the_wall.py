440bb43116b54fd82db54ccc520a7974349c0be1
Barak Ben-David <barakbd@live.com>
2016-07-16 18:44:29 -0700

    working wall - need to add reset pwd

--- the_wall.py
+++ the_wall.py
@@ -16,8 +16,33 @@ PASSWORD_REGEX=re.compile(r'^([0-9]+[a-zA-Z]+|[a-zA-Z]+[0-9]+)[0-9a-zA-Z]*$')
 
 @app.route('/')
 def index():
-    return render_template('login_reg.html')
+    print session
+    key_exists=session.get('user_id')
+    if key_exists:
+        return redirect ('/view_wall')
+    else:
+        return render_template('index.html')
 
+
+######################## SHOW WALL #############################################
+
+@app.route('/view_wall')
+def view_wall():
+    print session
+    if 'user_id' not in session:
+        flash ('NONE SHALL PASS!', 'login')
+        return redirect('/')
+    else:
+        print 'showing wall'
+        print session
+        query_posts = mysql.query_db('SELECT * FROM posts LEFT JOIN users ON users_user_id=user_id ORDER BY posts.created_at DESC;')
+        query_comments = mysql.query_db('SELECT * from comments LEFT JOIN users ON users_user_id=user_id ORDER BY comments.created_at DESC')
+        print 'posts', query_posts,'\n'
+        print 'comments', query_comments, '\n'
+        #### object returned is query_posts #########
+        return render_template('/the_wall.html', query_posts=query_posts, query_comments=query_comments)
+
+######################## REGISTER #############################################
 @app.route('/register', methods=['POST'])
 def register():
     print 'info received - ', request.form
@@ -27,12 +52,14 @@ def register():
     email=request.form['email']
     password=request.form['password']
     password_verify=request.form['password_verify']
-
+    ###################### SESSION FOR TESTING #################################
     session['first_name'] = request.form['first_name']
     session['last_name'] = request.form['last_name']
     session['email'] = request.form['email']
     session['password'] = request.form['password']
     session['password_verify'] = request.form['password_verify']
+    ###################### CHECK FIELDS #################################
+
     print first_name, last_name, email, password, password_verify
 
     required_fields=True
@@ -75,6 +102,7 @@ def register():
         flash("passwords don't match", 'register')
         valid=False
 
+    ###################### IF ALL FIELD ARE VALID #################################
     if valid==True:
         pw_hash = bcrypt.generate_password_hash(password)
         data_registered = {
@@ -85,11 +113,20 @@ def register():
                         }
         query_check_email_exists = 'SELECT email from users where email = :email'
         query_email_compare = mysql.query_db(query_check_email_exists, data_registered)
-
         if not query_email_compare:
-            query_insert_new_user = 'INSERT INTO users (first_name, last_name, email, pw_hash) Values (:first_name, :last_name, :email, :pw_hash)'
+            query_insert_new_user = 'INSERT INTO users (first_name, last_name, email, pw_hash, created_at, updated_at) Values (:first_name, :last_name, :email, :pw_hash, NOW(), NOW())'
             mysql.query_db(query_insert_new_user, data_registered)
+
+            ########## CREATE SESSION FOR WALL#############
+            query_new_user = 'SELECT * from users WHERE email = :email'
+            data_new_email = {'email': email}
+            query_for_session_data = mysql.query_db(query_new_user, data_new_email)
+            session['user_id']=query_for_session_data[0]['user_id']
+            session['first_name']=query_for_session_data[0]['first_name']
+            session['last_name']=query_for_session_data[0]['last_name']
+            print session
             flash('Registration Success!', 'register')
+            return redirect ('/view_wall')
         else:
             flash('Email Exists!', 'register')
             print 'email exists'
@@ -97,6 +134,8 @@ def register():
         return redirect('/')
     else:
         return redirect('/')
+
+
 
 
 
@@ -126,7 +165,7 @@ def login():
 
     if valid==True:
         print 'session is', session
-        query_login_data = 'SELECT email, pw_hash FROM users WHERE email = :login_email'
+        query_login_data = 'SELECT user_id, first_name, last_name, email, pw_hash FROM users WHERE email = :login_email'
         login_data = {
                     'login_email': request.form['login_email']
                     }
@@ -144,14 +183,58 @@ def login():
             valid=False
 
         if valid == True:
+            session['user_id']=login_verification[0]['user_id']
+            session['first_name']=login_verification[0]['last_name']
+            session['last_name']=login_verification[0]['last_name']
+            print session
             flash('Login Success!', 'login')
-            print 'success'
-            return redirect ('/')
+            print 'Login success'
+            return redirect ('/view_wall')
         else:
             return redirect ('/')
 
     else:
         return redirect ('/')
+######################## LOGIN #############################################
+@app.route('/reset_password', methods=['POST'])
+def reset_password_submit():
+    return redirect('/')
+######################## POST #############################################
+@app.route('/new_post', methods=['POST'])
+def new_post():
+    session.get('new_post_text', request.form['new_post'])
+    print 'trying to post'
+    print session
+
+    query_input_new_post = 'INSERT INTO posts (post_text, users_user_id, created_at, updated_at) VALUES (:new_post_text, :user_id, NOW(), NOW())'
+    data_new_post = {
+                    'new_post_text': request.form['new_post'],
+                    'user_id': session['user_id']
+                    }
+    mysql.query_db(query_input_new_post, data_new_post)
+    print 'New post added to db'
+    return redirect('/view_wall')
+
+######################## COMMENT #############################################
+@app.route('/new_comment/<post_id>', methods=['POST'])
+def new_comment(post_id):
+    session.get('new_comment_text', request.form['new_comment'])
+    print 'trying to comment'
+    print session
+
+    query_input_new_comment = 'INSERT INTO comments (comment_text, users_user_id, created_at, updated_at, posts_post_id) VALUES (:new_comment_text, :user_id, NOW(), NOW(), :post_id)'
+    data_new_comment = {
+                    'new_comment_text': request.form['new_comment'],
+                    'user_id': session['user_id'],
+                    'post_id': post_id
+                    }
+    print "starting query to input comment"
+    mysql.query_db(query_input_new_comment, data_new_comment)
+    print 'New comment added to db'
+    return redirect('/view_wall')
+
+
+######################## LOGOUT #############################################
 
 @app.route('/clear_session')
 def clear_session():
